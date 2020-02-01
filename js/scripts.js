4816a29235765a8e7eb581aee3088847c8b275fa
nellagio <nickgallo41@gmail.com>
2020-01-31 16:49:06 -0800

    console.log issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,33 @@
-//front-end
+//Front-back
+function updatePrice() {
+    var priceValue = calculatePrice(); 
+    
+    var priceField = document.getElementById("totalCost");
+    var priceDiv = document.getElementById("showPrice");
+     priceField.innerHTML = "$" + priceValue;
+    }  
 
-// $(document).ready(function () {
+function calculatePrice() {
+    var orderForm = document.getElementById("orderForm");
+    var price = 0;
+    // get size of pizza
 
-// }
+    price = price + parseInt(orderForm.size.value);
 
-//backend
+    // get price of meats
+    var meatOptions = orderForm.meat;
+    for (var i = 0; i < meatOptions.length; i++) {
+        if (meatOptions[i].checked) {
+            price = price + 2;
+        }
+    }
+
+    // get price of veggies
+    var vegOptions = orderForm.veggie;
+    for (var i = 0; i < vegOptions.length; i++) {
+        if (vegOptions[i].checked) {
+            price = price + 1;
+        }
+    }
+    return price;
+}
